bc3d25b7b362d5c545a3ab65816a649463ce4eed
JeffersonXamir <46076279+JeffersonXamir@users.noreply.github.com>
2026-07-16 11:52:36 -0500

    servidor moodle ip fija para server revisar la copia del servidor de ubuntu

--- app/src/main/java/ec/edu/ug/moodleug/utils/Constants.java
+++ app/src/main/java/ec/edu/ug/moodleug/utils/Constants.java
@@ -2,7 +2,7 @@ package ec.edu.ug.moodleug.utils;
 
 public class Constants {
     // URL del servidor (Usa la IP de tu PC si pruebas en un dispositivo físico)
-    public static final String MOODLE_BASE_URL = "http://192.168.1.4/moodle/";//"http://labdigital.com/moodle/";//"http://10.0.2.2/moodle/";
+    public static final String MOODLE_BASE_URL = "http://192.168.1.47/moodle/";
 
     // Token temporal para pruebas de desarrollo (El que generaste en el Paso 4 de Moodle)
     public static final String MOODLE_TEST_TOKEN = "0c36a2a4a804aee1787991db2fe2d155";
